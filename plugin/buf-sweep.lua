c19f35fe21853d937735480afc2aaca276cfa101
mogulla3 <hs.iarn3@gmail.com>
2022-11-10 23:34:49 +0900

    Run stylua

--- plugin/buf-sweep.lua
+++ plugin/buf-sweep.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command("BufSweep", function(args)
-  local buf_sweep = require('buf-sweep')
+  local buf_sweep = require("buf-sweep")
   if args.bang then
     buf_sweep.sweep({ force = true })
   else
